b5e9d333f532570dbefb35c86ab3e55ebbcb6023
Martyncalderon <89615487+Martyncalderon@users.noreply.github.com>
2022-10-05 13:16:37 -0500

    Add files via upload

---
+++ Ecuacion/Taller-Testing-master/M/src/main/java/com/bbva/taller/Calculadora.java
@@ -0,0 +1,35 @@
+package com.bbva.taller;
+
+public class Calculadora
+{
+	
+	private Extractor extractor = new Extractor();
+	
+	
+	/**
+	 * Comentario de prueba
+	 * @param cadena
+	 * @return
+	 */
+	public int dimeElResultadoDe(final String cadena)
+	{
+		final int[] operandos = extractor.extraerOperandos(cadena);
+
+		final String operador = extractor.extraerOperador(cadena);
+		int resultado = 0;
+
+
+		switch (operador.charAt(0))
+		{
+			case '+':
+				resultado = operandos[0] + operandos[1];
+				break;
+			case '-':
+				resultado = operandos[0] - operandos[1];
+				break;
+		}
+
+		return resultado;
+	}
+
+}
